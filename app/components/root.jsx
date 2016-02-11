de6e3af8fb46726cf31966fc234430d1eea7e486
wbp <wbpiggot@gmail.com>
2016-02-10 23:20:48 -0800

    More work with babel, adding in React

---
+++ app/components/root.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Bar from '/Users/ben/SeattleMapProject/lib/components/bar';
+
+const Yo = React.createClass({
+  render() {
+    return <div> Yo!!! </div>
+  }
+})
+
+export default React.createClass({
+  render() {
+    return <div>
+        <div>Hello Ben</div>
+        <Yo />
+        <Bar />
+      </div>
+  }
+});
--- app/index.js
+++ app/index.js
@@ -1,8 +1,29 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-
+import hbs from 'express-handlebars';
+import React from 'react';
+import Root from './components/root';
 const app = express();
+const router = express.Router();
+
+
+app.engine('html', hbs({ extname: 'html' }));
+app.set('view engine', 'html');
+app.set('views', '/Users/ben/SeattleMapProject/app/views');
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get('/', (req, res) => {
+  res.render('index', { 
+    reactHtml: React.renderToString(<Root />)
+   });
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started ...")
+})
 
 export default app;
 
