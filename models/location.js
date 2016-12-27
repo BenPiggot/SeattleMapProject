9d1892aa285504e06df4fe56181bd1dc57b4ecd0
ben <wbpiggot@gmail.com>
2016-12-26 22:00:20 -0800

    Update geocoder npm package

--- models/location.js
+++ models/location.js
@@ -1,6 +1,7 @@
-var geocoder = require('geocoder');
-
 'use strict';
+
+var geocoder = require('node-geocoder');
+
 module.exports = function(sequelize, DataTypes) {
   var location = sequelize.define('location', {
     name: DataTypes.STRING,
@@ -14,17 +15,17 @@ module.exports = function(sequelize, DataTypes) {
         // associations can be defined here
         models.location.belongsToMany(models.map, { through: models.mapslocations })
       }
+    },
+    hooks: {
+      beforeCreate: function(location, options, fn) {
+        geocoder.geocode(location, function(err, data) {
+          if (err) return fn(err, null);
+          location.latitude = data.results[0].geometry.location.lat;
+          location.longitude = data.results[0].geometry.location.lng;
+          fn(null, location);
+        });
+      }
     }
-    // hooks: {
-    //   beforeCreate: function(location, options, fn) {
-    //     geocoder.geocode(location, function(err, data) {
-    //       if (err) return fn(err, null);
-    //       location.latitude = data.results[0].geometry.location.lat;
-    //       location.longitude = data.results[0].geometry.location.lng;
-    //       fn(null, location);
-    //     });
-    //   }
-    // }
   });
   return location;
 };
--- models/map.js
+++ models/map.js
@@ -1,4 +1,7 @@
 'use strict';
+
+var geocoder = require('node-geocoder');
+
 module.exports = function(sequelize, DataTypes) {
   var map = sequelize.define('map', {
     topic: DataTypes.STRING,
@@ -11,6 +14,16 @@ module.exports = function(sequelize, DataTypes) {
       associate: function(models) {
         // associations can be defined here
         models.map.belongsToMany(models.location, { through: models.mapslocations })
+      }
+    },
+    hooks: {
+      beforeCreate: function(map, options, fn) {
+        geocoder.geocode(map, function(err, data) {
+          if (err) return fn(err, null);
+          map.latitude = data.results[0].geometry.map.lat;
+          map.longitude = data.results[0].geometry.map.lng;
+          fn(null, map;
+        });
       }
     }
   });
